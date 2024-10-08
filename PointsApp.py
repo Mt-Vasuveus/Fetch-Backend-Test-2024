050ea4df2cd05176257532fbf1e35d52eb9edb17
Mt-Vasuveus <vasujanjrukia+github@gmail.com>
2024-10-08 11:11:07 -0500

    Updated database query function

--- PointsApp.py
+++ PointsApp.py
@@ -24,15 +24,17 @@ def get_db():
 # Create necessary structure in database
 def init_db():
     with app.app_context():
-        init_query = "CREATE TABLE IF NOT EXISTS pointsdb(payer TEXT, points INTEGER, timestamp TEXT);"
-        query_db(init_query)
+        init_points_query = "CREATE TABLE IF NOT EXISTS points_db(payer TEXT, points INTEGER, timestamp TEXT);"
+        query_db(init_points_query)
 
     return
 
 # From Flask Documentation https://flask.palletsprojects.com/en/3.0.x/patterns/sqlite3/
 def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
+    db = get_db()
+    cur = db.execute(query, args)
     results = cur.fetchall()
+    db.commit()
     cur.close()
     return (results[0] if results else None) if one else results
 
@@ -55,13 +57,12 @@ def add_point():
             "timestamp" not in request_json.keys()):
         return "Invalid JSON", 400
     
-    add_query = "INSERT INTO pointsdb VALUES (?, ?, ?);"
+    add_query = "INSERT INTO points_db VALUES (?, ?, ?);"
     try:
         query_db(add_query, [
             request_json["payer"], 
             request_json["points"], 
             request_json["timestamp"]])
-        g._database.commit()
         return "", 200
     except Error as e:
         return "Error adding points", 500
@@ -74,7 +75,7 @@ def spend_points():
 
 @app.route("/balance", methods=['GET'])
 def get_balance():
-    balance_query = "SELECT payer, points FROM pointsdb;"
+    balance_query = "SELECT payer, points FROM points_db;"
     balance = query_db(balance_query)
 
     print(balance)
