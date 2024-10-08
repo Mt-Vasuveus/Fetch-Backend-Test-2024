1541b00bbe8b04af85799afa8ccc9fd71a99fdb8
Mt-Vasuveus <vasujanjrukia+github@gmail.com>
2024-10-08 10:13:04 -0500

    Created /add API

--- PointsApp.py
+++ PointsApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, g
+from flask import Flask, request, jsonify, g
 import sqlite3
 from sqlite3 import Error
 
@@ -24,8 +24,9 @@ def get_db():
 # Create necessary structure in database
 def init_db():
     with app.app_context():
-        init_query = "CREATE TABLE IF NOT EXISTS points(payer TEXT, points INTEGER, timestamp TEXT)"
+        init_query = "CREATE TABLE IF NOT EXISTS pointsdb(payer TEXT, points INTEGER, timestamp TEXT);"
         query_db(init_query)
+
     return
 
 # From Flask Documentation https://flask.palletsprojects.com/en/3.0.x/patterns/sqlite3/
@@ -38,25 +39,47 @@ def query_db(query, args=(), one=False):
 @app.teardown_appcontext
 def close_db(exception):
     db = getattr(g, "_database", None)
-    if db is None:
+    if db is not None:
         db.close()
-
-
 
 ########################
 # API Calls
 ########################
 @app.route("/add", methods=['POST'])
 def add_point():
-    print("adding points")
+    request_json = request.get_json()
+
+    # Check that the json has the data we need
+    if ("payer" not in request_json.keys() or
+            "points" not in request_json.keys() or 
+            "timestamp" not in request_json.keys()):
+        return "Invalid JSON", 400
+    
+    add_query = "INSERT INTO pointsdb VALUES (?, ?, ?);"
+    try:
+        query_db(add_query, [
+            request_json["payer"], 
+            request_json["points"], 
+            request_json["timestamp"]])
+        g._database.commit()
+        return "", 200
+    except Error as e:
+        return "Error adding points", 500
+
 
 @app.route("/spend", methods=['POST'])
 def spend_points():
     print("spending points")
+    return "WIP"
 
 @app.route("/balance", methods=['GET'])
 def get_balance():
+    balance_query = "SELECT payer, points FROM pointsdb;"
+    balance = query_db(balance_query)
+
+    print(balance)
     print("getting points")
+    return "WIP"
 
 
 ########################
