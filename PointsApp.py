bc583991393961f09c3d32d4881229245800f681
Mt-Vasuveus <vasujanjrukia+github@gmail.com>
2024-10-08 16:22:45 -0500

    Added type checking for JSON Fixed return type for /spend Wrapped database accesses in try block

--- PointsApp.py
+++ PointsApp.py
@@ -38,6 +38,7 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (results[0] if results else None) if one else results
 
+# Make sure we close the database on killing server
 @app.teardown_appcontext
 def close_db(exception):
     db = getattr(g, "_database", None)
@@ -56,6 +57,11 @@ def add_point():
             "points" not in request_json.keys() or 
             "timestamp" not in request_json.keys()):
         return "Invalid JSON", 400
+
+    if (not isinstance(request_json["payer"], str) or
+            not isinstance(request_json["points"], int) or
+            not isinstance(request_json["timestamp"], str)):
+        return "Invalid JSON", 400
     
     add_query = "INSERT INTO points_db VALUES (?, ?, ?);"
     try:
@@ -70,19 +76,23 @@ def add_point():
 
 @app.route("/spend", methods=['POST'])
 def spend_points():
-    print("spending points")
-
     request_json = request.get_json()
 
     # Check that the json has the data we need
     if ("points" not in request_json.keys()):
         return "Invalid JSON", 400
 
+    if (not isinstance(request_json["points"], int)):
+        return "Invalid JSON", 400
+
     points_to_spend = request_json["points"]
     
     # Check if there are enough points to spend
-    total_balance_query = "SELECT SUM(points) FROM points_db"
-    total_balance = query_db(total_balance_query)[0][0]
+    try:
+        total_balance_query = "SELECT SUM(points) FROM points_db"
+        total_balance = query_db(total_balance_query)[0][0]
+    except:
+        return "Error spending points", 500
 
     if (total_balance < points_to_spend):
         return "Insufficient points", 400
@@ -91,50 +101,57 @@ def spend_points():
     running_total = dict()
 
     while points_to_spend > 0:
-        # Makes sure we get the oldest query that isn't 0
-        # Have to keep 0s to make sure we still display payers with 0 points in /balance
-        oldest_query = "SELECT * FROM points_db WHERE points != 0 ORDER BY timestamp ASC;"
-        oldest_points = query_db(oldest_query, one = True)
+        try:
+            # Makes sure we get the oldest query that isn't 0
+            # Have to keep 0s to make sure we still display payers with 0 points in /balance
+            oldest_query = "SELECT * FROM points_db WHERE points != 0 ORDER BY timestamp ASC;"
+            oldest_points = query_db(oldest_query, one = True)
 
-        # Make sure we can keep track of who's points we spent
-        if (oldest_points[0] not in running_total.keys()):
-            running_total[oldest_points[0]] = 0
 
-        if (oldest_points[1] > points_to_spend):
+            # Make sure we can keep track of who's points we spent
+            if (oldest_points[0] not in running_total.keys()):
+                running_total[oldest_points[0]] = 0
+
             # Not all points will be spent
-            update_query = "UPDATE points_db SET points = ? WHERE payer = ? AND points = ? AND timestamp = ?;"
-            query_db(update_query, [
-                oldest_points[1] - points_to_spend,
-                oldest_points[0],
-                oldest_points[1],
-                oldest_points[2]])
+            if (oldest_points[1] > points_to_spend):
+                update_query = "UPDATE points_db SET points = ? WHERE payer = ? AND points = ? AND timestamp = ?;"
+                query_db(update_query, [
+                    oldest_points[1] - points_to_spend,
+                    oldest_points[0],
+                    oldest_points[1],
+                    oldest_points[2]])
 
-            # Update how much we spent for a given buyer
-            running_total[oldest_points[0]] -= points_to_spend
-            # If we're not spending all of the points, we're done spending
-            return
-        else:
+                # Update how much we spent for a given buyer
+                running_total[oldest_points[0]] -= points_to_spend
+                # If we're not spending all of the points, we're done spending
+                return jsonify(running_total)
             # Set entry in points_db to 0
-            update_query = "UPDATE points_db SET points = ? WHERE payer = ? AND points = ? AND timestamp = ?;"
-            query_db(update_query, [
-                0,
-                oldest_points[0],
-                oldest_points[1],
-                oldest_points[2]])
+            else:
+                update_query = "UPDATE points_db SET points = ? WHERE payer = ? AND points = ? AND timestamp = ?;"
+                query_db(update_query, [
+                    0,
+                    oldest_points[0],
+                    oldest_points[1],
+                    oldest_points[2]])
 
-            # Update how much we spent for a given buyer
-            running_total[oldest_points[0]] -= oldest_points[1]
-            # Update the ammount we still need to spend 
-            points_to_spend -= oldest_points[1]
+                # Update how much we spent for a given buyer
+                running_total[oldest_points[0]] -= oldest_points[1]
+                # Update the ammount we still need to spend 
+                points_to_spend -= oldest_points[1]
 
-    return jsonify(running_total), 200
+        except:
+            return "Error spending points", 500
+
+    return jsonify(running_total)
 
 @app.route("/balance", methods=['GET'])
 def get_balance():
-    balance_query = "SELECT payer, SUM(points) FROM points_db GROUP BY payer;"
-    balance = query_db(balance_query)
+    try:
+        balance_query = "SELECT payer, SUM(points) FROM points_db GROUP BY payer;"
+        balance = query_db(balance_query)
+    except:
+        return "Error getting balance", 500
 
-    print(balance)
     return jsonify(balance), 200
 
 
